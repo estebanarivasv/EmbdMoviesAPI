423030f4b3990fee28a6fd2086cfa76d233ffc0c
Esteban Rivas <estebanarivasv@gmail.com>
2021-05-05 09:49:33 -0300

    Upload backend's initial files

---
+++ backend.java/trailers-db/src/test/java/com/sword/trailersdb/TrailersDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sword.trailersdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailersDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
