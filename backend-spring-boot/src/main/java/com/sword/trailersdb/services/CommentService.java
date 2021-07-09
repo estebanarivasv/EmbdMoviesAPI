529e345af2ee0f37fcac3f9817b9e8350e0e15b4
Esteban Rivas <estebanarivasv@gmail.com>
2021-07-09 15:27:41 -0300

    Initial services files

---
+++ backend-spring-boot/src/main/java/com/sword/trailersdb/services/CommentService.java
@@ -0,0 +1,16 @@
+package com.sword.trailersdb.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+    private void add() {}
+
+    private void modify() {}
+
+    private void delete() {}
+
+    private void get() {}
+
+}
---
+++ backend-spring-boot/src/main/java/com/sword/trailersdb/services/UserService.java
@@ -0,0 +1,12 @@
+package com.sword.trailersdb.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private void modifyUser() {}
+
+    private void getUser() {}
+
+}
