21a5078ea34d5750e690480457e21a7cab9e4ff0
tebu0 <estebanarivasv@gmail.com>
2021-10-20 14:51:09 -0300

    Fix endpoints

--- src/main/java/com/erivas/embd/utilities/constants/Endpoints.java
+++ src/main/java/com/erivas/embd/utilities/constants/Endpoints.java
@@ -3,15 +3,19 @@ package com.erivas.embd.utilities.constants;
 public class Endpoints {
 
     public static final String API_VERSION = "/api/v1";
-    public static final String GLOBAL_COMMENTS = API_VERSION + "/comments";
-    public static final String GLOBAL_MOVIES = API_VERSION + "/movies";
-    public static final String GLOBAL_USERS = API_VERSION + "/users";
+    public static final String ALL_ACTOR = API_VERSION + "/actors";
+    public static final String ALL_DIRECTOR = API_VERSION + "/directors";
+    public static final String ALL_MOVIES = API_VERSION + "/movies";
+    public static final String ALL_PLAYLISTS = API_VERSION + "/playlists";
 
+    public static final String SPECIFIC_ACTOR = API_VERSION + "/actor/{id}";
     public static final String SPECIFIC_COMMENT = API_VERSION + "/comment/{id}";
+    public static final String SPECIFIC_DIRECTOR = API_VERSION + "/movie/{id}";
     public static final String SPECIFIC_MOVIE = API_VERSION + "/movie/{id}";
+    public static final String SPECIFIC_PLAYLIST = API_VERSION + "/playlist/{id}";
     public static final String SPECIFIC_USER = API_VERSION + "/user/{id}";
 
-    public static final String COMMENTS_IN_MOVIE = API_VERSION + "/comment/{commentId}/movie/{movieId}";
+    public static final String COMMENTS_FROM_MOVIE = SPECIFIC_MOVIE + "/comments";
 
     public Endpoints() {
     }
